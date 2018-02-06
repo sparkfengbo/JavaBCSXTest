4566b8b34d3672747ab1610db080241af78fd9ef
fengbo <fengbo04@baidu.com>
2018-02-06 15:39:04 +0800

    no

---
+++ app/src/main/java/com/sparkfengbo/app/javabcsxtest/ExportedActivity.java
@@ -0,0 +1,16 @@
+package com.sparkfengbo.app.javabcsxtest;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.SystemClock;
+
+public class ExportedActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+}
